a4026e00d432c9ffbba16612ef3fc08f2719efd8
SergDeikun <dsv2807@gmail.com>
2022-10-03 16:50:52 +0300

    Update add.js

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -3,6 +3,7 @@ const { Contact } = require("../../models");
 const add = async (req, res, next) => {
   try {
     const newContact = await Contact.create(req.body);
+    console.log(newContact);
 
     res.status(201).json(newContact);
   } catch (error) {
